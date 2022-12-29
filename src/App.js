1d20856755fb1d5989bbf7ef8a61528245930677
Glen Geteri <glennenri24@gmail.com>
2022-12-29 15:23:21 +0300

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ProfilePage from './pages/ProfilePage';
+import DashboardPage from './pages/DashboardPage';
+import NavBar from './components/nav/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/profile' element={<ProfilePage/>}/>
+        <Route path='/admin' element={<DashboardPage/>}/>
+      </Routes>
     </div>
   );
 }
